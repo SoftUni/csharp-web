1a0bf699a48b1737b9a3eaef580b2367bd543b26
Nikolay Kostov <admin@nikolay.it>
2020-01-15 10:00:12 +0200

    First lecture demo - HttpRequester

---
+++ 2020-Jan-Season/HttpRequester/HttpRequester/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HttpRequester
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            const string NewLine = "\r\n";
+            TcpListener tcpListener = new TcpListener(IPAddress.Loopback, 80);
+            tcpListener.Start();
+            while (true)
+            {
+                TcpClient tcpClient = tcpListener.AcceptTcpClient();
+                using NetworkStream networkStream = tcpClient.GetStream();
+                byte[] requestBytes = new byte[1000000]; // TODO: Use buffer
+                int bytesRead = networkStream.Read(requestBytes, 0, requestBytes.Length);
+                string request = Encoding.UTF8.GetString(requestBytes, 0, bytesRead);
+                string responseText = @"<form action='/Account/Login' method='post'>
+<input type=date name='date' />
+<input type=text name='username' />
+<input type=password name='pasword' />
+<input type=submit value='Login' />
+</form>";
+                string response = "HTTP/1.0 200 OK" + NewLine +
+                                  "Server: SoftUniServer/1.0" + NewLine +
+                                  "Content-Type: text/html" + NewLine +
+                                  // "Location: https://google.com" + NewLine +
+                                  // "Content-Disposition: attachment; filename=niki.html" + NewLine +
+                                  "Content-Lenght: " + responseText.Length + NewLine +
+                                  NewLine +
+                                  responseText;
+                byte[] responseBytes = Encoding.UTF8.GetBytes(response);
+                networkStream.Write(responseBytes, 0, responseBytes.Length);
+                Console.WriteLine(request);
+                Console.WriteLine(new string('=', 60));
+            }
+        }
+
+        public static async Task HttpRequest()
+        {
+            HttpClient client = new HttpClient();
+            HttpResponseMessage response = await client.GetAsync("https://softuni.bg/");
+            string result = await response.Content.ReadAsStringAsync();
+            File.WriteAllText("index.html", result);
+        }
+    }
+}
