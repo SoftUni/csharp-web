d81a7599878e294c8a0edb5418b453c4fa99d4b5
Nikolay Kostov <admin@nikolay.it>
2020-01-17 22:12:58 +0200

    HttpRequester after the "Asynchronous Programming" lecture

--- 2020-Jan-Season/HttpRequester/HttpRequester/Program.cs
+++ 2020-Jan-Season/HttpRequester/HttpRequester/Program.cs
@@ -4,6 +4,7 @@ using System.Net;
 using System.Net.Http;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace HttpRequester
@@ -12,43 +13,43 @@ namespace HttpRequester
     {
         static async Task Main(string[] args)
         {
-            const string NewLine = "\r\n";
             TcpListener tcpListener = new TcpListener(IPAddress.Loopback, 80);
             tcpListener.Start();
             while (true)
             {
-                TcpClient tcpClient = tcpListener.AcceptTcpClient();
-                using NetworkStream networkStream = tcpClient.GetStream();
-                byte[] requestBytes = new byte[1000000]; // TODO: Use buffer
-                int bytesRead = networkStream.Read(requestBytes, 0, requestBytes.Length);
-                string request = Encoding.UTF8.GetString(requestBytes, 0, bytesRead);
-                string responseText = @"<form action='/Account/Login' method='post'>
+                TcpClient tcpClient = await tcpListener.AcceptTcpClientAsync();
+#pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+                Task.Run(() => ProcessClientAsync(tcpClient));
+#pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+            }
+        }
+
+        private static async Task ProcessClientAsync(TcpClient tcpClient)
+        {
+            const string NewLine = "\r\n";
+            using NetworkStream networkStream = tcpClient.GetStream();
+            byte[] requestBytes = new byte[1000000]; // TODO: Use buffer
+            int bytesRead = await networkStream.ReadAsync(requestBytes, 0, requestBytes.Length);
+            string request = Encoding.UTF8.GetString(requestBytes, 0, bytesRead);
+            string responseText = @"<form action='/Account/Login' method='post'>
 <input type=date name='date' />
 <input type=text name='username' />
 <input type=password name='pasword' />
 <input type=submit value='Login' />
-</form>";
-                string response = "HTTP/1.0 200 OK" + NewLine +
-                                  "Server: SoftUniServer/1.0" + NewLine +
-                                  "Content-Type: text/html" + NewLine +
-                                  // "Location: https://google.com" + NewLine +
-                                  // "Content-Disposition: attachment; filename=niki.html" + NewLine +
-                                  "Content-Lenght: " + responseText.Length + NewLine +
-                                  NewLine +
-                                  responseText;
-                byte[] responseBytes = Encoding.UTF8.GetBytes(response);
-                networkStream.Write(responseBytes, 0, responseBytes.Length);
-                Console.WriteLine(request);
-                Console.WriteLine(new string('=', 60));
-            }
-        }
-
-        public static async Task HttpRequest()
-        {
-            HttpClient client = new HttpClient();
-            HttpResponseMessage response = await client.GetAsync("https://softuni.bg/");
-            string result = await response.Content.ReadAsStringAsync();
-            File.WriteAllText("index.html", result);
+</form>" + "<h1>" + DateTime.UtcNow + "</h1>";
+            Thread.Sleep(10000);
+            string response = "HTTP/1.0 200 OK" + NewLine +
+                              "Server: SoftUniServer/1.0" + NewLine +
+                              "Content-Type: text/html" + NewLine +
+                              // "Location: https://google.com" + NewLine +
+                              // "Content-Disposition: attachment; filename=niki.html" + NewLine +
+                              "Content-Lenght: " + responseText.Length + NewLine +
+                              NewLine +
+                              responseText;
+            byte[] responseBytes = Encoding.UTF8.GetBytes(response);
+            await networkStream.WriteAsync(responseBytes, 0, responseBytes.Length);
+            Console.WriteLine(request);
+            Console.WriteLine(new string('=', 60));
         }
     }
 }
