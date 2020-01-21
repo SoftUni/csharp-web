6d712c68b26340cdcf61234b4d262996f829233c
Nikolay Kostov <admin@nikolay.it>
2020-01-21 20:44:29 +0200

    Changes from the "Cookies and Session Management" lecture

--- 2020-Jan-Season/HttpRequester/HttpRequester/Program.cs
+++ 2020-Jan-Season/HttpRequester/HttpRequester/Program.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Net;
 using System.Net.Http;
 using System.Net.Sockets;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -11,6 +13,7 @@ namespace HttpRequester
 {
     class Program
     {
+        static Dictionary<string, int> SessionStore = new Dictionary<string, int>();
         static async Task Main(string[] args)
         {
             TcpListener tcpListener = new TcpListener(IPAddress.Loopback, 80);
@@ -31,16 +34,32 @@ namespace HttpRequester
             byte[] requestBytes = new byte[1000000]; // TODO: Use buffer
             int bytesRead = await networkStream.ReadAsync(requestBytes, 0, requestBytes.Length);
             string request = Encoding.UTF8.GetString(requestBytes, 0, bytesRead);
-            string responseText = @"<form action='/Account/Login' method='post'>
-<input type=date name='date' />
-<input type=text name='username' />
-<input type=password name='pasword' />
-<input type=submit value='Login' />
-</form>" + "<h1>" + DateTime.UtcNow + "</h1>";
-            Thread.Sleep(10000);
+
+            var sid = Regex.Match(request, @"sid=[^\n]*\n").Value?.Replace("sid=", string.Empty).Trim();
+            Console.WriteLine(sid);
+            var newSid = Guid.NewGuid().ToString();
+            var count = 0;
+            if (SessionStore.ContainsKey(sid))
+            {
+                SessionStore[sid]++;
+                count = SessionStore[sid];
+            }
+            else
+            {
+                sid = null;
+                SessionStore[newSid] = 1;
+                count = 1;
+            }
+
+
+            string responseText = "<h1>" + count + "</h1>" + "<h1>" + DateTime.UtcNow + "</h1>";
             string response = "HTTP/1.0 200 OK" + NewLine +
                               "Server: SoftUniServer/1.0" + NewLine +
                               "Content-Type: text/html" + NewLine +
+                              "Set-Cookie: user=Niki; Max-Age: 3600; HttpOnly;" + NewLine +
+                              (string.IsNullOrWhiteSpace(sid) ?
+                                ("Set-Cookie: sid=" + newSid + NewLine)
+                                : string.Empty) +
                               // "Location: https://google.com" + NewLine +
                               // "Content-Disposition: attachment; filename=niki.html" + NewLine +
                               "Content-Lenght: " + responseText.Length + NewLine +
