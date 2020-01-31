18a367395600eb815b215da61fb6831af5df18e8
radrex <radostinv.stoychev@gmail.com>
2020-02-01 00:35:31 +0200

    Add documentation for HtmlResponse.cs

--- 2020-Jan-Season/SoftUni-Information-Services/SIS/SIS.HTTP/Response/HtmlResponse.cs
+++ 2020-Jan-Season/SoftUni-Information-Services/SIS/SIS.HTTP/Response/HtmlResponse.cs
@@ -1,9 +1,16 @@
-﻿using System.Text;
-
-namespace SIS.HTTP.Response
+﻿namespace SIS.HTTP.Response
 {
+    using System.Text;
+
+    /// <summary>
+    /// Represents an HTML Response with properties for the <c>Response Status Line</c>, <c>Response Headers</c> and <c>Response Body</c>.
+    /// </summary>
     public class HtmlResponse : HttpResponse
     {
+        /// <summary>
+        /// Initializes a new <see cref="HtmlResponse"/> class.
+        /// </summary>
+        /// <param name="html">HTML text</param>
         public HtmlResponse(string html)
         {
             this.StatusCode = HttpResponseCode.Ok;
