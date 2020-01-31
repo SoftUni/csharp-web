444f4bccb7738ed3b3428f0f202b56a52c3c4828
radrex <radostinv.stoychev@gmail.com>
2020-02-01 00:32:50 +0200

    Add documentation for IHttpServer.cs

--- 2020-Jan-Season/SoftUni-Information-Services/SIS/SIS.HTTP/IHttpServer.cs
+++ 2020-Jan-Season/SoftUni-Information-Services/SIS/SIS.HTTP/IHttpServer.cs
@@ -1,14 +1,25 @@
-﻿using System.Threading.Tasks;
-
-namespace SIS.HTTP
+﻿namespace SIS.HTTP
 {
+    using System.Threading.Tasks;
 
+    /// <summary>
+    /// Describes HTTP Server functionalities.
+    /// </summary>
     public interface IHttpServer
     {
+        /// <summary>
+        /// Starts the HTTP Server asynchronously.
+        /// </summary>
         Task StartAsync();
 
+        /// <summary>
+        /// Resets the HTTP Server asynchronously.
+        /// </summary>
         Task ResetAsync();
 
+        /// <summary>
+        /// Stops the HTTP Server.
+        /// </summary>
         void Stop();
     }
 }
