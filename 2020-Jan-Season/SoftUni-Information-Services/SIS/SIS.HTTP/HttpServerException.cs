e0ed84ade6dd222e8ef6018276b20db7366ad104
radrex <radostinv.stoychev@gmail.com>
2020-02-01 00:32:02 +0200

    Add documentation for HttpServerException.cs

--- 2020-Jan-Season/SoftUni-Information-Services/SIS/SIS.HTTP/HttpServerException.cs
+++ 2020-Jan-Season/SoftUni-Information-Services/SIS/SIS.HTTP/HttpServerException.cs
@@ -1,9 +1,16 @@
-﻿using System;
-
-namespace SIS.HTTP
+﻿namespace SIS.HTTP
 {
+    using System;
+
+    /// <summary>
+    /// Represents errors that occur during HTTP Request and HTTP Response parsing.
+    /// </summary>
     public class HttpServerException : Exception
     {
+        /// <summary>
+        /// Initializes a new instance of <see cref="HttpServerException"/> class with the specified error message.
+        /// </summary>
+        /// <param name="message">The message that describes the error.</param>
         public HttpServerException(string message)
             : base(message)
         {
