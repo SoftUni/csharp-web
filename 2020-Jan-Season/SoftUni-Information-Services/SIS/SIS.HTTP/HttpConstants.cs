39c8e4d7f72f58e64680981efb67c39033eec20c
radrex <radostinv.stoychev@gmail.com>
2020-02-01 00:28:56 +0200

    Add documentation for HttpConstants.cs

--- 2020-Jan-Season/SoftUni-Information-Services/SIS/SIS.HTTP/HttpConstants.cs
+++ 2020-Jan-Season/SoftUni-Information-Services/SIS/SIS.HTTP/HttpConstants.cs
@@ -1,5 +1,8 @@
 ﻿namespace SIS.HTTP
 {
+    /// <summary>
+    /// Contains various HTTP Constants.
+    /// </summary>
     public static class HttpConstants
     {
         public const string NewLine = "\r\n";
