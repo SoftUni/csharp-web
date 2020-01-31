e67658165bd8d2b6a8ac3e04d7b3bbbf497680c4
radrex <radostinv.stoychev@gmail.com>
2020-02-01 00:34:56 +0200

    Add documentation for FileResponse.cs

--- 2020-Jan-Season/SoftUni-Information-Services/SIS/SIS.HTTP/Response/FileResponse.cs
+++ 2020-Jan-Season/SoftUni-Information-Services/SIS/SIS.HTTP/Response/FileResponse.cs
@@ -1,11 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace SIS.HTTP.Response
+﻿namespace SIS.HTTP.Response
 {
+    /// <summary>
+    /// Represents a File Response with properties for the <c>Response Status Line</c>, <c>Response Headers</c> and <c>Response Body</c>.
+    /// </summary>
     public class FileResponse : HttpResponse
     {
+        /// <summary>
+        /// Initializes a new <see cref="FileResponse"/> class.
+        /// </summary>
+        /// <param name="fileContent">File bytes</param>
+        /// <param name="contentType">MIME Content type</param>
         public FileResponse(byte[] fileContent, string contentType)
         {
             this.StatusCode = HttpResponseCode.Ok;
