b95522e3fa5ee3b3f7dae89370b598d68a0da16a
radrex <radostinv.stoychev@gmail.com>
2020-02-01 00:27:33 +0200

    Add documentation for Cookie.cs

--- 2020-Jan-Season/SoftUni-Information-Services/SIS/SIS.HTTP/Cookie.cs
+++ 2020-Jan-Season/SoftUni-Information-Services/SIS/SIS.HTTP/Cookie.cs
@@ -1,15 +1,31 @@
 ﻿namespace SIS.HTTP
 {
+    /// <summary>
+    /// Represents an HTTP Cookie information, consisting of <c>Name</c> and <c>Value</c>.
+    /// </summary>
     public class Cookie
     {
+        //------------- CONSTRUCTORS -------------
+        /// <summary>
+        /// Initializes a new <see cref="Cookie"/> class.
+        /// </summary>
+        /// <param name="name">Cookie name</param>
+        /// <param name="value">Cookie value</param>
         public Cookie(string name, string value)
         {
             this.Name = name;
             this.Value = value;
         }
 
+        //-------------- PROPERTIES --------------
+        /// <summary>
+        /// HTTP Cookie Name.
+        /// </summary>
         public string Name { get; set; }
 
+        /// <summary>
+        /// HTTP Cookie Value.
+        /// </summary>
         public string Value { get; set; }
     }
 }
