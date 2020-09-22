f175a50641ca1b733e5d399a7c9a63e6fc677ce3
Nikolay Kostov <admin@nikolay.it>
2020-09-22 17:12:45 +0300

    Upload demos after Async lecture

--- 2020-Sept-Season/BasicHttpServer/HttpClientDemo/Program.cs
+++ 2020-Sept-Season/BasicHttpServer/HttpClientDemo/Program.cs
@@ -1,53 +1,67 @@
 ﻿using System;
 using System.Collections.Concurrent;
+using System.Diagnostics;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace HttpClientDemo
 {
     class Program
     {
+        const string NewLine = "\r\n";
         static async Task Main(string[] args)
         {
             Console.OutputEncoding = Encoding.UTF8;
-            const string NewLine = "\r\n";
             TcpListener tcpListener = new TcpListener(
                 IPAddress.Loopback, 80);
             tcpListener.Start();
             while (true)
             {
                 var client = tcpListener.AcceptTcpClient();
-                using (var stream = client.GetStream())
-                {
-                    byte[] buffer = new byte[1000000];
-                    var lenght = stream.Read(buffer, 0, buffer.Length);
+                ProcessClientAsync(client);
+            }
+        }
 
-                    string requestString =
-                        Encoding.UTF8.GetString(buffer, 0, lenght);
-                    Console.WriteLine(requestString);
+        public static async Task ProcessClientAsync(TcpClient client)
+        {
+            using (var stream = client.GetStream())
+            {
+                byte[] buffer = new byte[1000000];
+                Console.WriteLine(Thread.CurrentThread.ManagedThreadId);
+                var lenght = await stream.ReadAsync(buffer, 0, buffer.Length);
+                Console.WriteLine(Thread.CurrentThread.ManagedThreadId);
 
-                    string html = $"<h1>Hello from NikiServer {DateTime.Now}</h1>" +
-                        $"<form action=/tweet method=post><input name=username /><input name=password />" +
-                        $"<input type=submit /></form>";
+                string requestString =
+                    Encoding.UTF8.GetString(buffer, 0, lenght);
+                Console.WriteLine(requestString);
 
-                    string response = "HTTP/1.1 200 OK" + NewLine +
-                        "Server: NikiServer 2020" + NewLine +
-                        // "Location: https://www.google.com" + NewLine +
-                        "Content-Type: text/html; charset=utf-8" + NewLine +
-                        // "Content-Disposition: attachment; filename=niki.txt" + NewLine +
-                        "Content-Lenght: " + html.Length + NewLine +
-                        NewLine +
-                        html + NewLine;
+                Thread.Sleep(5000);
 
-                    byte[] responseBytes = Encoding.UTF8.GetBytes(response);
-                    stream.Write(responseBytes);
+                string html = $"<h1>Hello from NikiServer {DateTime.Now}</h1>" +
+                    $"<form action=/tweet method=post><input name=username /><input name=password />" +
+                    $"<input type=submit /></form>" + DateTime.Now;
 
-                    Console.WriteLine(new string('=', 70));
-                }
+                string response = "HTTP/1.1 200 OK" + NewLine +
+                    "Server: NikiServer 2020" + NewLine +
+                    // "Location: https://www.google.com" + NewLine +
+                    "Content-Type: text/html; charset=utf-8" + NewLine +
+                    "X-Server-Version: 1.0" + NewLine +
+                    // "Content-Disposition: attachment; filename=niki.txt" + NewLine +
+                    "Content-Lenght: " + html.Length + NewLine +
+                    NewLine +
+                    html + NewLine;
+
+                byte[] responseBytes = Encoding.UTF8.GetBytes(response);
+                Console.WriteLine(Thread.CurrentThread.ManagedThreadId);
+                await stream.WriteAsync(responseBytes);
+                Console.WriteLine(Thread.CurrentThread.ManagedThreadId);
+
+                Console.WriteLine(new string('=', 70));
             }
         }
 
@@ -55,6 +69,8 @@ namespace HttpClientDemo
         {
             string url = "https://softuni.bg/courses/csharp-web-basics";
             HttpClient httpClient = new HttpClient();
+            // httpClient.DefaultRequestHeaders.Add("X-Client-Version", "1.0");
+            Console.WriteLine(Thread.CurrentThread.ManagedThreadId);
             var response = await httpClient.GetAsync(url);
             Console.WriteLine(response.StatusCode);
             Console.WriteLine(string.Join(Environment.NewLine,
