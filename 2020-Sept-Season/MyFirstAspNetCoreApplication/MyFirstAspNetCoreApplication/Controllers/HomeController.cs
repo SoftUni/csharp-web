dcaa5d8d2ad25d2fb70244052b119693e0f72a9b
Nikolay Kostov <admin@nikolay.it>
2020-11-13 19:42:34 +0200

    AJAX demo in MyFirstAspNetCoreApplication

--- 2020-Sept-Season/MyFirstAspNetCoreApplication/MyFirstAspNetCoreApplication/Controllers/HomeController.cs
+++ 2020-Sept-Season/MyFirstAspNetCoreApplication/MyFirstAspNetCoreApplication/Controllers/HomeController.cs
@@ -69,6 +69,20 @@ namespace MyFirstAspNetCoreApplication.Controllers
             return View();
         }
 
+        public IActionResult AjaxDemo()
+        {
+            return this.View();
+        }
+
+        public IActionResult AjaxDemoData()
+        {
+            return this.Json(new[] {
+                new { Name = "Niki2", Date = DateTime.UtcNow.ToString("O") },
+                new { Name = "Stoyan2", Date = DateTime.UtcNow.AddDays(1).ToString("O") },
+                new { Name = "Pesho2", Date = DateTime.UtcNow.AddDays(2).ToString("O") },
+            });
+        }
+
         [HttpPost]
         public IActionResult ContactForm(string title, string content)
         {
